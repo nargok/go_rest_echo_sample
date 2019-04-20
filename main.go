fdd5d8d09d36e7b7a5f11de62428c3a45dfd5a02
nargok <nargok@gmail.com>
2019-04-20 22:25:37 +0900

    comment list

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+type Comment struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+	Text string `json:"text"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
@@ -22,5 +28,14 @@ func main() {
 		return c.String(http.StatusOK, "Hello "+name)
 	})
 
+	e.GET("/comments", getComments)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getComments(c echo.Context) error {
+	comments := []Comment{Comment{1, "Test comment", "This is Test"}}
+	comment2 := Comment{Id: 2, Name: "Test comment2", Text: "This is Test2"}
+	comments = append(comments, comment2)
+	return c.JSON(http.StatusOK, comments)
 }
